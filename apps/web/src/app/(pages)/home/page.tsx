f005d1cfdcef5f56e205cd6d1ed2a6ad644d3f90
emzrivera <elamarizrivera@gmail.com>
2025-08-12 20:40:03 +0800

    setup assets (fonts and colors) for web and mobile

---
+++ apps/web/src/app/(pages)/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="bg-dark">
+  <h1 className="text-4xl font-figtree bg-darkest text-white p-4">Dormease</h1>
+  <p className="font-montserrat text-xl">Home Page</p>
+</div>
+  )
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+// app/page.tsx
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+  redirect('/home');
+}
+
